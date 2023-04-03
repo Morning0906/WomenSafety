a32b4a50462d844847508f0a391f6405331a4fa5
Morning0906 <morning000906@163.com>
2023-04-03 21:54:18 +0800

    update_0403_2

--- web/src/main.js
+++ web/src/main.js
@@ -4,8 +4,12 @@ import {
 import App from '@/App.vue'
 import router from '@/router'
 import store from "@/store";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const forumApp = createApp(App)
 forumApp.use(router)
     .use(store)
     .mount('#app')
+
+forumApp.use(ElementPlus)
